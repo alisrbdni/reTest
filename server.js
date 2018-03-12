c58c680bf387a4ff85ccf8820ae13e9fc9ffd66e
alisrbdni <sarabadaniali@yahoo.com>
2018-03-13 00:29:24 +1100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express()
 var async = require('async');
 var resStr='';
      function checkIfDivisible(results){
-      obj = results[0];
-      obj2 = results[1];
+      obj = results.one;
+      obj2 = results.two;
        //loop through between range provided by first api 
        for(var i=obj.lower;i<obj.upper+1;i++){
          resStr+=(i!=0?i+':':'');
@@ -17,16 +17,13 @@ var resStr='';
        resStr+='<br>';    
        }
      }
-   
 
 app.get('/', (req, res) => {
-//using async parallel to run both funcs with callbacks 
-//and store in results arr
-      async.parallel([
-        /*
-         * range API
-         */
-        function(callback) {
+//defining two functions in one parallelFunctions object
+var parallelFunctions = {
+
+        //range API
+        one: function(callback) {
           var url = "https://join.reckon.com/test1/rangeInfo";
           request(url, function(err, response, body) {
             // JSON body
@@ -35,10 +32,9 @@ app.get('/', (req, res) => {
             callback(false, obj);
           });
         },
-        /*
-         * divisor API
-         */
-        function(callback) {
+
+        // divisor API
+        two:function(callback) {
           var url = "https://join.reckon.com/test1/divisorInfo";
           request(url, function(err, response, body) {
             // JSON body
@@ -47,19 +43,27 @@ app.get('/', (req, res) => {
             callback(false, obj);
           });
         },
-      ],
-      /*
-       * calling checkIfDivisible as logic and sending resStr as response 
-       */
-      function(err, results) {
-        if(err) { console.log(err); res.send(500,"Server Error"); return; }
-        checkIfDivisible(results);
-        res.send(resStr);
-        resStr ='';
       }
-      );
-    // }
-  
-  })
 
-app.listen(9999, () => console.log('Reckon test1 app listening on port 9999'))
+      //calling both apis in parallel using async parallel
+      var doThemInParallel = function(callback) {
+        async.parallel(parallelFunctions, function(err, results) {
+            callback(err, results);
+        });
+    };
+
+    //using async retry with number of tries and intervals in between till response is a success 
+    async.retry({
+      times: 20,
+      interval: 2000
+    }, doThemInParallel, function(err, results) {
+
+    //calling checkIfDivisible as logic and sending resStr as response 
+      if(err) { console.log(err); res.send(500,"Server Error"); return; }
+      checkIfDivisible(results);
+      res.send(resStr);
+      resStr ='';
+    });
+  })
+  
+  app.listen(9999, () => console.log('Reckon test1 app listening on port 9999'))
